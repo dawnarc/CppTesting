f3714002390573d970de32d6934f4ead301b9dba
dawnarc <aiguo1024@gmail.com>
2019-09-08 22:49:31 +0800

    add case: type checking & cast

--- Test01/Main.cpp
+++ Test01/Main.cpp
@@ -75,33 +75,107 @@ class C4
 	virtual void fun1() {}
 };
 
+class C5
+{
+	char a;
+	char b;
+	int c;
+
+	virtual void fun1() = 0;
+};
+
 //Byte Alignment
 void _20190905()
 {
-	int size1 = sizeof(C1);
-	printf("C1 size:%d\n", size1);
+	printf("C1 size:%d\n", sizeof(C1));
+	printf("C2 size:%d\n", sizeof(C2));
+	printf("C3 size:%d\n", sizeof(C3));
+	printf("C4 size:%d\n", sizeof(C4));
+	printf("C5 size:%d\n", sizeof(C5));
 
-	int size2 = sizeof(C2);
-	printf("C2 size:%d\n", size2);
+	/*output(x64):
+	C1 size:8
+	C2 size:12
+	C3 size:8
+	C4 size:16
+	C5 size:16
+	*/
 
-	int size3 = sizeof(C3);
-	printf("C3 size:%d\n", size3);
-
-	int size4 = sizeof(C4);
-	printf("C4 size:%d\n", size4);
-
-	/*output:
-	C1 size : 8
-	C2 size : 12
-	C3 size : 8
-	C4 size : 16
+	/*output(x86):
+	C1 size:8
+	C2 size:12
+	C3 size:8
+	C4 size:12
+	C5 size:12
 	*/
 }
 
-int main(int argc, char* [])
+class CA01
+{
+	int a;
+};
+
+class CA02 : public CA01
+{
+	char* b;
+};
+
+class CA03 : public CA01
+{
+	float c;
+};
+
+class CB01
+{
+	char d;
+};
+
+class CC01
+{
+	long e;
+
+	virtual void fun01() {}
+};
+
+void _20190908()
+{
+	CA02 obj01;
+
+	CA02* ptr01 = &obj01;
+
+	CA03* ptr02 = (CA03*)(ptr01);
+	printf("%d\n", ptr02 ? 1 : 0);	//output:1
+
+	//error C2440:  'static_cast': cannot convert from 'CA02 *' to 'CA03 *'
+	//CA03* ptr03 = static_cast<CA03*>(ptr01);
+
+	//error C2683:  'dynamic_cast': 'CA02' is not a polymorphic type
+	//CA03* ptr04 = dynamic_cast<CA03*>(ptr01);
+	
+	CA01* ptr05 = dynamic_cast<CA01*>(ptr01);
+	printf("%d\n", ptr05 ? 1 : 0);	//output:1
+
+	CA03* ptr06 = static_cast<CA03*>(ptr05);
+	printf("%d\n", ptr06 ? 1 : 0);	//output:1
+
+	CC01 obj02;
+	CC01* ptr07 = &obj02;
+
+	CA01* ptr08 = dynamic_cast<CA01*>(ptr07);
+	printf("%d\n", ptr08 ? 1 : 0);	//output:0
+
+	//error C2440:  'static_cast': cannot convert from 'CC01 *' to 'CA01 *'
+	//CA01* ptr09 = static_cast<CA01*>(ptr07);
+
+	CA01* ptr10 = (CA01*)ptr07;
+	printf("%d\n", ptr10 ? 1 : 0);	//output:1
+}
+
+int main(int argc, char* args[])
 {
 	//_20190824();
-	_20190905();
+	//_20190905();
+	_20190908();
 
 	system("pause");
 
